ca1e95133b7ebac6a31fa9575476d6d11e0c7e65
João Neto <joaoneto@baratocoletivo.com.br>
2019-11-30 21:39:17 -0300

    deploy: public dir

---
+++ public/_nuxt/3c456457190bb821bc02.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{385:function(t,e,n){"use strict";n.r(e);var r={},c=n(51),component=Object(c.a)(r,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h2",[this._v("Vaga")])])}],!1,null,null,null);e.default=component.exports}}]);
